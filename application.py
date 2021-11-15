3bca88ac5489a9f4dffab8a556c34075e0c49acc
Denis Duval <denis.duval@telecom-paris.fr>
2021-11-15 18:20:56 +0100

    first commit

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import numpy as np
+
+application = Flask(__name__)
+
+@application.route('/')
+def hello():
+    return 'Hello DENIS DUVAL\n'
+    
+@application.route('/random/<n>')
+def randomvalues(n):
+    values = np.random.randint(0, 10, int(n))
+    result = {'values' : values.tolist()}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    application.run(debug=True)
